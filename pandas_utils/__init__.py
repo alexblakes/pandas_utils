9a76e4474dd2b5cfe6a67b622d424d5dc08cecad
Alex <alex.blakes@gmail.com>
2025-12-09 23:15:05 +0000

    Add defaults for pandas checks

--- pandas_utils/__init__.py
+++ pandas_utils/__init__.py
@@ -1,4 +1,15 @@
+import logging
+
 import pandas as pd
+import pandas_checks as pdc
 
 from .method_chains import flatten_columns, assign_with_apply
 
+# Logging
+logger = logging.getLogger(__name__)
+format = "[%(asctime)s] %(levelname)s | %(message)s "
+logging.basicConfig(level=logging.DEBUG, format=format)
+
+# Pandas checks
+pdc.set_custom_print_fn(logger.info, print_to_stdout=False)
+pdc.set_format(precision=3, use_emojis=False)
